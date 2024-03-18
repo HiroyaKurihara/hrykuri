3fe46e4320472a3f72b6a517e0483bdff01127fd
HiroyaKurihara <157329629+HiroyaKurihara@users.noreply.github.com>
2024-03-18 15:59:21 +0900

    portfolioBrushUp

---
+++ portfolio/creation/ccdonuts/ccdonuts/footer.php
@@ -0,0 +1,21 @@
+<?php
+echo '<div class="fback">
+        <div class="footerNavigation">
+            <p class="footerNaviFrame">よくある質問</p>
+            <p class="footerNaviFrame">お問い合わせ</p>
+            <p class="footerNaviFrame">当サイトのポリシー</p>
+        </div>
+        <div class="snsLogo">
+            <img src="images/Facebook.webp" class="facebook">
+            <img src="images/Instagram.png" class="instagram">
+            <img src="images/twiiter.svg" class="twitter">
+        </div>
+        <div class="logoFooter"></div>
+        <p class="copyright">Copyright (C) 2023 c.c.donuts</p>
+    </div>
+</body>
+?>
+</html>';
+?>
+</body>
+</html>
---
+++ portfolio/creation/ccdonuts/ccdonuts/index.php
@@ -0,0 +1,56 @@
+<?php require 'header.php'; ?>
+<?php
+echo '<header></header>
+  <main>
+    <p class="welcome">ようこそ　ゲスト様</p><img src="images/hero.png" class="hero">
+    <div><span><a class="newSell">新商品</a><a class="summerCitrusText"></a><img src="images/guide.png"
+          class="summerCitrus"></span><img src="images/donutLife.png" class="donutLife"></div><img
+      src="images/banner.png" class="banner">
+    <div class="philosophy"></div>
+    <p class="rankingTitle">人気ランキング</p>
+    <div class="rankingHorizontal">
+      <span class="rankingLeft">
+        <p>1</p><img src="images/ccDonutTopPage&ProductList.png" class="rankingImg">
+        <p class="rankingText">CC ドーナツ 当店オリジナル（5個入り）</p>
+        <p class="zeikomi">税込 ￥1,500</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span><span class="rankingRight">
+        <p>2</p><img src="images/fruitDonutAssortment1.png" class="rankingImg">
+        <p class="rankingText">フルーツドーナツセット（12個入り）</p>
+        <p class="zeikomi">税込 ￥3,500</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+    </div>
+    <div class="rankingHorizontal">
+      <span class="rankingLeft">
+        <p>3</p><img src="images/fruitDonutAssortment2.png" class="rankingImg">
+        <p class="rankingText">フルーツドーナツセット（14個入り）</p>
+        <p class="zeikomi">税込 ￥4,000</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="rankingRight">
+        <p>4</p><img src="images/chocolate.png" class="rankingImg">
+        <p class="rankingText">チョコレートデライト</p>
+        <p class="zeikomi">税込 ￥1,600</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+    </div>
+    <div class="rankingHorizontal">
+      <span class="rankingLeft">
+        <p>5</p><img src="images/bestBox.png" class="rankingImg">
+        <p class="rankingText">ベストセレクションボックス（4個入り）</p>
+        <p class="zeikomi">税込 ￥1,200</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="rankingRight">
+        <p>6</p><img src="images/strawberryCrush.png" class="rankingImg">
+        <p class="rankingText">ストロベリークラッシュ（5個入り）</p>
+        <p class="zeikomi">税込 ￥1,800</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+    </div>
+  </main>
+  <footer></footer>
+  <script></script>';
+  ?>
+<?php require 'footer.php'; ?>
