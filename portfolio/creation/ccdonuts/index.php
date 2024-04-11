3900ecb0f0bda2098ef14c015f906d99d93812e8
HiroyaKurihara <157329629+HiroyaKurihara@users.noreply.github.com>
2024-04-11 14:56:32 +0900

    Add files via upload

---
+++ portfolio/creation/ccdonuts/index.php
@@ -0,0 +1,82 @@
+<?php require 'header.php'; ?>
+<?php
+echo '<header></header>
+<main>
+<p class="welcome">ようこそ　ゲスト様</p><img src="images/hero.png" srcset="images/hero.png 375w, images/PChero.png 1366w"
+  sizes="(max-width: 767px) 375px, 1366px" alt="Hero Image" class="hero">
+<div class="sellAndLife"><span><a class="newSell">新商品</a><a class="summerCitrusText">サマーシトラス</a><img
+      src="images/guide.png" srcset="images/guide.png 375w, images/PCguide.png 1366w" alt="Guide Image"
+      class="summerCitrus" sizes="(max-width: 767px) 375px, 1366px"></span><span><img src="images/donutLife.png"
+      srcset="images/donutLife.png 375w, images/PCdonutLife.png 1366w" alt="Donut Life Image" class="donutLife"
+      sizes="(max-width: 767px) 375px, 1366px"><a class="donutLifeText">ドーナツのある生活</a></span></div>
+<div class="bannerDiv"><img src="images/banner.png" srcset="images/banner.png 375w, images/PCbanner.png 1366w"
+    alt="Banner Image" class="banner" sizes="(max-width: 767px) 375px, 1366px">
+  <p class="bannerText">商品一覧</p>
+</div>
+<div class="philosophy">
+  <p class="philosophyText">Philosophy</p>
+  <p class="ourMind">私たちの信念</p>
+  <p class="creatingConnections">"Creating Connections"</p>
+  <p class="connectDeDonut">「ドーナツでつながる」</p>
+</div>
+<div class="popularityRanking">
+  <div class="midashi">
+    <p class="rankingTitle">人気ランキング</p>
+  </div>
+  <div class="rankingOctopus">
+    <div class="rankingHorizontal">
+      <span class="rankingLeft">
+        <p class="rankLogo1">1</p><img src="images/ccDonutTopPage&ProductList.png"
+          srcset="images/ccDonutTopPage&ProductList.png 375w, images/PCccDonutTopPage&ProductList.png 1366w"
+          alt="Ranking Image" class="rankingImg" sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">CC ドーナツ 当店オリジナル（5個入り）</p>
+        <p class="zeikomi">税込 ￥1,500</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span><span class="rankingRight">
+        <p class="rankLogo2">2</p><img src="images/fruitDonutAssortment1.png"
+          srcset="images/fruitDonutAssortment1.png 375w, images/PCfruitDonutAssortment1.png 1366w"
+          alt="Ranking Image" class="rankingImg" sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">フルーツドーナツセット（12個入り）</p>
+        <p class="zeikomi">税込 ￥3,500</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="rankingLeft">
+        <p class="rankLogo3">3</p><img src="images/fruitDonutAssortment2.png"
+          srcset="images/fruitDonutAssortment2.png 375w, images/PCfruitDonutAssortment2.png 1366w"
+          alt="Ranking Image" class="rankingImg" sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">フルーツドーナツセット（14個入り）</p>
+        <p class="zeikomi">税込 ￥4,000</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="octopusBR"></span><span class="rankingRight">
+        <p class="rankLogo4">4</p><img src="images/chocolate.png"
+          srcset="images/chocolate.png 375w, images/PCchocolateProductList.png 1366w" alt="Chocolate Product Image"
+          class="rankingImg" sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">チョコレートデライト（5個入り）</p>
+        <p class="zeikomi">税込 ￥1,600</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="rankingLeft">
+        <p class="rankLogo5">5</p><img src="images/bestBox.png"
+          srcset="images/bestBox.png 375w, images/PCbestBox.png 1366w" alt="Best Box Image" class="rankingImg"
+          sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">ベストセレクションボックス（4個入り）</p>
+        <p class="zeikomi">税込 ￥1,200</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+      <span class="rankingRight">
+        <p class="rankLogo6">6</p><img src="images/strawberryCrush.png"
+          srcset="images/strawberryCrush.png 375w, images/PCstrawberryCrush.png 1366w" alt="Strawberry Crush Image"
+          class="rankingImg" sizes="(max-width: 767px) 375px, 1366px">
+        <p class="rankingText">ストロベリークラッシュ（5個入り）</p>
+        <p class="zeikomi">税込 ￥1,800</p>
+        <p class="shootInCart">カートに入れる</p>
+      </span>
+    </div>
+  </div>
+</div>
+</main>
+  <footer></footer>
+  <script></script>';
+  ?>
+<?php require 'footer.php'; ?>
